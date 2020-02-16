ef43772365dfb53da3cabb6c952c64051475a50d
Uzytkownik <uzytkownik@kursGIT.pl>
2020-02-16 10:53:29 +0100

    zmiana w Maturity

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import javax.swing.text.DateFormatter;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Urodzony w tej dacie - ");
+        if(maturity(17,2,1987))
+            System.out.println("pełnoletni");
+        else
+            System.out.println("niepełnoletni");
 
+    }
+
+    public static boolean maturity (int day, int month, int year){
+
+        LocalDate dateOfBirth, now;
+        dateOfBirth=LocalDate.of(year, month, day);
+
+
+        now = LocalDate.now();
+        Period p = Period.between(dateOfBirth, now);
+
+        if(p.getYears() >= 18) return true;
+        return false;
 
     }
 }
